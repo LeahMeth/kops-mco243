068648dcb486840d8d5896e7b5650b66e6516548
leahkops <leahkops1@gmail.com>
2016-03-11 02:04:13 -0500

    compiler

--- src/kops/compiler/Compiler.java
+++ src/kops/compiler/Compiler.java
@@ -1,5 +1,27 @@
 package kops.compiler;
 
+import java.io.BufferedReader;
+
 public class Compiler {
 
+	private StringBuilder machineCode;
+	
+	
+	
+	public Compiler(BufferedReader reader) {
+		machineCode = new StringBuilder();
+		index = 0;
+		
+		
+		
+		
+		
+	}
+
+	public String getMachineCode() {
+		return 
+	}
+
+	
+	
 }
--- src/kops/compiler/RunCompiler.java
+++ src/kops/compiler/RunCompiler.java
@@ -1,10 +1,22 @@
 package kops.compiler;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class RunCompiler {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(
+					"assemblyLanguage.txt"));
+			Compiler compiler = new Compiler(reader);
+			System.out.println(compiler.getMachineCode());				
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
